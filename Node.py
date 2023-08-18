f9ff7953be626b0d06aa9fe38e9476feb40a46a9
Magno Leal de Brito Junior <CIANDT\magnobrito@lnb024243cps.ciandt.global>
2023-08-18 09:40:01 -0300

    Creating the method drawTree using python turtle

--- Node.py
+++ Node.py
@@ -1,26 +1,55 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self,data,turtle):
         self.data = data
         self.left = None
         self.right = None
+        self.turtle = turtle
 
     def insert(self, value):
         if self.data is None:
-            self.data = data
+            self.data = value
         else:
             if value < self.data:
                 if self.left is None:
-                    self.left = Node(value)
+                    self.left = Node(value,self.turtle)
                 else:
                     self.left.insert(value)
             elif value > self.data:
                 if self.right is None:
-                    self.right = Node(value)
+                    self.right = Node(value,self.turtle)
                 else:
                     self.right.insert(value)
 
-        
+    def inOrderPrint(self):
+        if self.data is None:
+            return
+        else:
+            self.inOrderPrint(self.left)
+            print(root.data)
+            self.inOrderPrint(self.right)
+    
+    def drawTree(self,length, node):
+            if(length<10):
+                return
+            else:
+                if node.data:
+                    self.turtle.forward(length)
+                    self.turtle.color("red")
+                    self.turtle.write(node.data, font=("arial",14,"bold"))
+                    self.turtle.color("black")
+                    self.turtle.left(30)
+                    if node.right:
+                        self.drawTree(3*length/4,node.right)
+                    self.turtle.right(45)
+                    if node.left:
+                        self.drawTree(3*length/4,node.left)
+                    self.turtle.left(15)
+                    self.turtle.backward(length)
+                else:
+                    return 
+
+            
 
 
-        
+            
 
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import turtle
 from Node import Node
+import random
+
 
 t = turtle.Turtle()
-t.speed(1)
+t.hideturtle()
+t.speed(2)
 
-node = Node(1)
-node.insert(2)
-node.insert(-1)
+node = Node(5,t)
+vetor = []
+for i in range(20):
+    value = random.randrange(20)
+    vetor.append(value)
+    node.insert(value)
 
-print(node.left.data)
-print(node.data)
-print(node.right.data)
+
+
+t.right(90)
+
+node.drawTree(100,node)
+    
+     
+
+
+          
+# tree(t,200,node)
+
 
 t.getscreen()._root.mainloop()  # <-- run the Tkinter main loop
