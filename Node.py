c3f3000c1c3f68486dffa1f7d75afab7452066d3
Magno Leal de Brito Junior <CIANDT\magnobrito@lnb024243cps.ciandt.global>
2023-08-22 10:37:09 -0300

    Changin colors

--- Node.py
+++ Node.py
@@ -24,8 +24,9 @@ class Node:
         self.turtle.setheading(0)
         self.turtle.penup()
         self.turtle.goto(x,y)
+        self.turtle.color('#101884')
         self.turtle.write(title,font=("arial",12,"bold"))
-        self.turtle.goto(x,y-20)
+        self.turtle.goto(x+18,y-20)
         
 
     def inOrderPrint(self,node):
@@ -34,7 +35,9 @@ class Node:
         else:
             self.inOrderPrint(node.left)
             if node:
-                self.turtle.write(node.data, font=("arial",12,"bold"))
+                self.turtle.setheading(270)
+                self.turtle.color('#195c84')
+                self.turtle.write(str(node.data) + ", ", font=("arial",10,"bold"))
                 self.turtle.forward(30)
             self.inOrderPrint(node.right)
 
@@ -43,7 +46,9 @@ class Node:
             return
         else:
             if node:
-                self.turtle.write(node.data, font=("arial",12,"bold"))
+                self.turtle.setheading(270)
+                self.turtle.color('#195c84')
+                self.turtle.write(str(node.data) + ", ", font=("arial",10,"bold"))
                 self.turtle.forward(30)
             self.preOrderPrint(node.left)
             self.preOrderPrint(node.right)
@@ -55,27 +60,35 @@ class Node:
             self.posOrderPrint(node.left)
             self.posOrderPrint(node.right)
             if node:
-                self.turtle.write(node.data, font=("arial",12,"bold"))
+                self.turtle.setheading(270)
+                self.turtle.color('#195c84')
+                self.turtle.write(str(node.data) + ", " , font=("arial",10,"bold"))
                 self.turtle.forward(30)
     
     
     def drawTree(self,length, node):
-        if node.data:
+        if node.data is not None:
             self.turtle.forward(length)
-            self.turtle.color("red")
-            self.turtle.write(node.data, font=("arial",12,"bold"))
+            self.turtle.color("#003FB3")
+            self.turtle.write(node.data, font=("arial",10,"bold"))
             self.turtle.color("black")
-            self.turtle.left(30)
+            self.turtle.left(15)
             if node.right:
                 self.drawTree(3*length/4,node.right)
-            self.turtle.right(60)
+            self.turtle.right(30)
             if node.left:
                 self.drawTree(3*length/4,node.left)
-            self.turtle.left(30)
+            self.turtle.left(15)
             self.turtle.backward(length)
+            
+            
         else:
             return 
-
+        
+    
+         
+        
+    
             
 
 
